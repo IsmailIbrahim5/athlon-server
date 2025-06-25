3a80cb80144568576a8cbf334e345471ea0f1aa7
ismailibrahim5 <ismaeel_ibrahem@outlook.com>
2025-06-25 19:48:00 +0300

    final commit

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const app = express(); 
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+ 
 const s3 = new S3Client({
 region: "eu-central-2", 
   endpoint: "https://s3.eu-central-2.wasabisys.com",
